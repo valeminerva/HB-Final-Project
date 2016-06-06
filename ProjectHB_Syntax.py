417046f737446625388ce5f5fa83d4c5aaf07e6f
Valentina Carlini <Valentina@Valentinas-MacBook-Air.local>
2016-06-06 21:12:52 +0200

    Added genitive dict

--- ProjectHB_Syntax.py
+++ ProjectHB_Syntax.py
@@ -8,7 +8,7 @@ def sentence():
 	splitted_sentence = user_sentence.split()
 	return splitted_sentence
 
-splitted_sentence = sentence()
+#splitted_sentence = sentence()
 #print splitted_sentence
 
 print "Thank you. What would you like to do?"
@@ -16,8 +16,9 @@ print "Thank you. What would you like to do?"
 des_pres_ind_1_conj = {"1st pers. sg.":"o", "2nd pers. sg.":"as", "3d pers. sg.":"at", "1st pers. pl.":"amus", "2nd pers. pl.":"atis", "3d pers. pl.":"ant"}
 des_subst_first_decl = {"1st decl. nom. sg.":"a", "1st decl. acc. sg.":"am", "1st decl. nom. pl.":"ae", "1st decl. acc. sg.":"as"}
 des_subst_second_decl = {"2nd decl. nom. sg.":"us", "2nd decl. acc. sg.":"um", "2nd decl. nom. pl.":"i", "2nd decl. acc. pl.":"os"}
-des_subst_nominat = {"1a decl":"a", "2a decl":"us", "2a decl -er":"er"}
-des_subst_accusat = {"1a decl":"am", "2a decl":"um", "2a decl -er": "em"}
+des_subst_nominat_sg = {"1a decl":"a", "2a decl":"us", "2a decl -er":"er"}
+des_subst_genit_sg = {"1a decl":"ae", "2a decl":"i"}
+des_subst_accusat_sg = {"1a decl":"am", "2a decl":"um", "2a decl -er": "em"}
 
 #print des_pres_ind_1_conj.values()
 #print splitted_sentence
@@ -37,23 +38,22 @@ def find_subject():
 	global des_subst_second_decl
 	splitted_sentence = sentence()
 	for word in splitted_sentence:
-			if word[-1] == "a" or word[-2:] in des_subst_nominat.values():
+			if word[-1] == "a" or word[-2:] in des_subst_nominat_sg.values():
 				print "The subject is %s" % (word)
 				return
 
 def find_object():
-	global splitted_sentence
 	splitted_sentence = sentence()
 	for word in splitted_sentence:
-				if word[-2:] in des_subst_accusat.values():
+				if word[-2:] in des_subst_accusat_sg.values():
 					print "The object is %s" % (word)
 					return
 
 def analyze_word():
 	word_to_analyze = raw_input("which word would you like to analyze?")
-	if word_to_analyze[-1] == "a" or word_to_analyze[-2:] in des_subst_nominat.values():
+	if word_to_analyze[-1] == "a" or word_to_analyze[-2:] in des_subst_nominat_sg.values():
 		print "%s is a noun. Nom. Sg." % (word_to_analyze)
-	elif word_to_analyze[-2:] in des_subst_accusat.values():
+	elif word_to_analyze[-2:] in des_subst_accusat_sg.values():
 		print "%s is a noun. Acc. Sg." % (word_to_analyze)
 	elif word_to_analyze[-2:] in des_pres_ind_1_conj.values():
 		print "%s is a verb. Pres. Ind." % (word_to_analyze)

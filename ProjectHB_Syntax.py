2f36dcdde5bf9b89c2036e1d3ecabf81c62cd450
Valentina Carlini <Valentina@Valentinas-MacBook-Air.local>
2016-06-06 21:03:01 +0200

    First version working!

--- ProjectHB_Syntax.py
+++ ProjectHB_Syntax.py
@@ -16,9 +16,11 @@ print "Thank you. What would you like to do?"
 des_pres_ind_1_conj = {"1st pers. sg.":"o", "2nd pers. sg.":"as", "3d pers. sg.":"at", "1st pers. pl.":"amus", "2nd pers. pl.":"atis", "3d pers. pl.":"ant"}
 des_subst_first_decl = {"1st decl. nom. sg.":"a", "1st decl. acc. sg.":"am", "1st decl. nom. pl.":"ae", "1st decl. acc. sg.":"as"}
 des_subst_second_decl = {"2nd decl. nom. sg.":"us", "2nd decl. acc. sg.":"um", "2nd decl. nom. pl.":"i", "2nd decl. acc. pl.":"os"}
+des_subst_nominat = {"1a decl":"a", "2a decl":"us", "2a decl -er":"er"}
+des_subst_accusat = {"1a decl":"am", "2a decl":"um", "2a decl -er": "em"}
 
-print des_pres_ind_1_conj.values()
-print splitted_sentence
+#print des_pres_ind_1_conj.values()
+#print splitted_sentence
 
 def find_verb():
 	global des_pres_ind_1_conj
@@ -26,70 +28,60 @@ def find_verb():
 	for word in splitted_sentence:
 		for ending in des_pres_ind_1_conj.values():
 			if word[-2:] in des_pres_ind_1_conj.values():
+				#print word [-2:]
 				print "The verb is %s" % (word)
 				return
 
+def find_subject():
+	global des_subst_first_decl
+	global des_subst_second_decl
+	splitted_sentence = sentence()
+	for word in splitted_sentence:
+			if word[-1] == "a" or word[-2:] in des_subst_nominat.values():
+				print "The subject is %s" % (word)
+				return
+
+def find_object():
+	global splitted_sentence
+	splitted_sentence = sentence()
+	for word in splitted_sentence:
+				if word[-2:] in des_subst_accusat.values():
+					print "The object is %s" % (word)
+					return
+
+def analyze_word():
+	word_to_analyze = raw_input("which word would you like to analyze?")
+	if word_to_analyze[-1] == "a" or word_to_analyze[-2:] in des_subst_nominat.values():
+		print "%s is a noun. Nom. Sg." % (word_to_analyze)
+	elif word_to_analyze[-2:] in des_subst_accusat.values():
+		print "%s is a noun. Acc. Sg." % (word_to_analyze)
+	elif word_to_analyze[-2:] in des_pres_ind_1_conj.values():
+		print "%s is a verb. Pres. Ind." % (word_to_analyze)
+
+def analyze_sentence():
+	find_verb_var = find_verb()
+	find_subject_var = find_subject()
+	find_object_var = find_object()
+	
 
 def menu():
-	print "1 - Find verb \n 2 - Find Subject \n 3 - Find Object \n 4 - Analyse word \n 5 - Analyse all \n 6 - Exit" 
+	print "1 - Find verb \n 2 - Find Subject \n 3 - Find Object \n 4 - Analyze word \n 5 - Analyze sentence \n 6 - Exit" 
 	user_choice = int(raw_input("Please, type the number of your choice."))
 	if user_choice == 1:
 		find_verb()
 	elif user_choice == 2:
-		subject()
+		find_subject()
 	elif user_choice == 3:
-		object()
+		find_object()
 	elif user_choice == 4:
-		word_to_analyse()
+		analyze_word()
 	elif user_choice == 5:
-		analyse_sentence()
+		analyze_sentence()
 	else:
 		 exit_analyzer()
 
 menu()
 
-# des_pres_ind = {"1st pers. sg.":"o", "2nd pers. sg.":"s", "3d pers. sg.":"t", "1st pers. pl.":"mus", "2nd pers. pl.":"tis", "3d pers. pl.":"nt"}
-# des_subst_first_decl = {"1st decl. nom. sg.":"a", "1st decl. acc. sg.":"am", "1st decl. nom. pl.":"ae", "1st decl. acc. sg.":"as"}
-# des_subst_second_decl = {"2nd decl. nom. sg.":"us", "2nd decl. acc. sg.":"um", "2nd decl. nom. pl.":"i", "2nd decl. acc. pl.":"os"}
-
-# def find_verb():
-# 	global des_pres_ind
-# 	splitted_sentence = sentence()
-# 	for word in splitted_sentence:
-# 		for ending in des_pres_ind.values():
-# 			if word[-2:] in des_pres_ind.values():
-# 				print "The verb is %s", (word)
-# 			else:
-# 				print "Ohoh. In this sentence there is no verb."
-
-
-# # #def subject():
-# # 	#pass
-# # 	# for che analizzi tutte le parole
-# # 	# if / elif :
-# # 	# se trova una parola che corrisponde
-# # 	# se trova soggetto singolare: continua a cercare: forse due soggetti!
-# # 	# se no il soggetto e implicito o il verbo e impersonale
-
-# def object():
-# 	global splitted_sentence
-# 	for word in splitted_sentence:
-# 		for ending in des_subst_first_decl:
-# 			for ending in des_subst_second_decl:
-# 				if word[-2:] in des_subst_first_decl or des_subst_second_decl:
-# 					print "Object is %s", (word)
-# 				else:
-# 					print "Ohoh. In this sentence there is no object."
-
-# object()
-
-# #def word_to_analyze():
-# 	#word_to_anayse = raw_input("What word do you want me to analyse for you?")
-
-# #def analyse_sentence():
-# 	#verb()
-# 	#subject()
-# 	#object()
 
 # #def exit_analyzer():
 # 	#pass

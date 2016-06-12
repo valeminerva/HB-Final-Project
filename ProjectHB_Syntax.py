dcb36e9d5276471007cafad1586f6214935d3663
Valentina Carlini <Valentina@Valentinas-MacBook-Air.local>
2016-06-13 01:42:16 +0200

    Added 4th conj ind pres and exit

--- ProjectHB_Syntax.py
+++ ProjectHB_Syntax.py
@@ -9,59 +9,82 @@ def sentence():
 
 print "Thank you. What would you like to do?"
 
-des_pres_ind_pres_1_conj = {"1st pers. sg. ind. pres.":"o", "2nd pers. sg. ind. pres.":"as", "3rd pers. sg. ind. pres.":"at", "1st pers. pl. ind. pres.":"amus", "2nd pers. pl. ind. pres.":"atis", "3d pers. pl. ind. pres.":"ant"}
-des_pres_ind_pres_2_conj = {"1st pers. sg. ind. pres.":"eo", "2nd pers. sg. ind. pres.":"es", "3rd pers. sg. ind. pres.":"et", "1st pers. pl. ind. pres.":"emus", "2nd pers. pl. ind. pres.":"etis", "3d pers. pl. ind. pres.":"ent"}
+des_ind_pres_1_conj = {"1st pers. sg. ind. pres.":"o", "2nd pers. sg. ind. pres.":"as", "3rd pers. sg. ind. pres.":"at", "1st pers. pl. ind. pres.":"amus", "2nd pers. pl. ind. pres.":"atis", "3d pers. pl. ind. pres.":"ant"}
+des_ind_pres_2_conj = {"1st pers. sg. ind. pres.":"eo", "2nd pers. sg. ind. pres.":"es", "3rd pers. sg. ind. pres.":"et", "1st pers. pl. ind. pres.":"emus", "2nd pers. pl. ind. pres.":"etis", "3d pers. pl. ind. pres.":"ent"}
+des_ind_pres_4_conj = {"1st pers. sg. ind. pres.":"io", "2nd pers. sg. ind. pres.":"is", "3rd pers. sg. ind. pres.":"it", "1st pers. pl. ind. pres.":"imus", "2nd pers. pl. ind. pres.":"itis", "3d pers. pl. ind. pres.":"iunt"}
+
 des_subst_first_decl = {"1st decl. nom. sg.":"a", "1st decl. acc. sg.":"am", "1st decl. nom. pl.":"ae", "1st decl. acc. sg.":"as"}
 des_subst_second_decl = {"2nd decl. nom. sg.":"us", "2nd decl. acc. sg.":"um", "2nd decl. nom. pl.":"i", "2nd decl. acc. pl.":"os"}
+
 des_subst_nominat = {"1a decl. nom. sg.":"a", "2a decl. nom. sg.":"us", "2a decl -er":"er", "1a decl. nom. pl.":"ae", "2a decl. nom. pl.":"i"}
 des_subst_genit = {"1a decl. gen. sg.":"ae", "2a decl. gen. sg.":"i", "1a decl. gen. pl.":"arum", "2a decl. gen. pl.":"orum"}
 des_subst_accusat = {"1a decl. acc. sg.":"am", "2a decl. acc. sg.":"um", "1a decl. acc. pl.":"as", "2a decl. acc. pl.":"os"}
 
 def find_verb():
-	find_verb_1st_conj()
-	find_verb_2nd_conj()	
+	find_verb_1st_conj_ind_pres()
+	find_verb_2nd_conj_ind_pres()
+	find_verb_4_conj_ind_pres()	
 print "There is no verb."
 
-def find_verb_1st_conj():
-	global des_pres_ind_1_conj
+def find_verb_1st_conj_ind_pres():
+	global des_ind_pres_1_conj
 	splitted_sentence = sentence()
 	for word in splitted_sentence:
-		for ending in des_pres_ind_pres_1_conj.values():
-			if word[-1] in des_pres_ind_pres_1_conj.values():
-				ending = des_pres_ind_pres_1_conj.keys()[des_pres_ind_pres_1_conj.values().index(word[-1])] 
+		for ending in des_ind_pres_1_conj.values():
+			if word[-1] in des_ind_pres_1_conj.values() and word[-2:] != "eo" and word[-2:] != "io":
+				ending = des_ind_pres_1_conj.keys()[des_ind_pres_1_conj.values().index(word[-1])] 
+				print "The verb is %s. It is a %s, 1st conj." % (word, ending)
+				return None
+			elif word[-2:] in des_ind_pres_1_conj.values():
+				ending = des_ind_pres_1_conj.keys()[des_ind_pres_1_conj.values().index(word[-2:])] 
+				print "The verb is %s. It is a %s, 1st conj." % (word, ending)
+				return None
+			elif word[-3:] in des_ind_pres_1_conj.values():
+				ending = des_ind_pres_1_conj.keys()[des_ind_pres_1_conj.values().index(word[-3:])] 
+				print "The verb is %s. It is a %s, 1st conj." % (word, ending)
+				return None
+			elif word[-4:] in des_ind_pres_1_conj.values():
+				ending = des_ind_pres_1_conj.keys()[des_ind_pres_1_conj.values().index(word[-4:])] 
+				print "The verb is %s. It is a %s, 1st conj." % (word, ending)
+				return None
+
+def find_verb_2nd_conj_ind_pres():
+	global des_ind_pres_2_conj
+	splitted_sentence = sentence()
+	for word in splitted_sentence:
+		for ending in des_ind_pres_2_conj.values():
+			if 	word[-2:] in des_ind_pres_2_conj.values():
+				ending = des_ind_pres_2_conj.keys()[des_ind_pres_2_conj.values().index(word[-2:])] 
 				print "The verb is %s. It is a %s." % (word, ending)
 				return None
-			elif word[-2:] in des_pres_ind_pres_1_conj.values():
-				ending = des_pres_ind_pres_1_conj.keys()[des_pres_ind_pres_1_conj.values().index(word[-2:])] 
+			elif word[-3:] in des_ind_pres_2_conj.values():
+				ending = des_ind_pres_2_conj.keys()[des_ind_pres_2_conj.values().index(word[-3:])] 
 				print "The verb is %s. It is a %s." % (word, ending)
 				return None
-			elif word[-3:] in des_pres_ind_pres_1_conj.values():
-				ending = des_pres_ind_pres_1_conj.keys()[des_pres_ind_pres_1_conj.values().index(word[-3:])] 
-				print "The verb is %s. It is a %s." % (word, ending)
-				return None
-			elif word[-4:] in des_pres_ind_pres_1_conj.values():
-				ending = des_pres_ind_pres_1_conj.keys()[des_pres_ind_pres_1_conj.values().index(word[-4:])] 
+			elif word[-4:] in des_ind_pres_2_conj.values():
+				ending = des_ind_pres_2_conj.keys()[des_ind_pres_2_conj.values().index(word[-4:])] 
 				print "The verb is %s. It is a %s." % (word, ending)
 				return None
 
-def find_verb_2nd_conj():
-	global des_pres_ind_pres_2_conj
+def find_verb_4_conj_ind_pres():
+	global des_ind_pres_4_conj
 	splitted_sentence = sentence()
 	for word in splitted_sentence:
-		for ending in des_pres_ind_pres_2_conj.values():
-			if 	word[-2:] in des_pres_ind_pres_2_conj.values():
-				ending = des_pres_ind_pres_2_conj.keys()[des_pres_ind_pres_2_conj.values().index(word[-2:])] 
+		for ending in des_ind_pres_4_conj.values():
+			if 	word[-2:] in des_ind_pres_4_conj.values():
+				ending = des_ind_pres_4_conj.keys()[des_ind_pres_4_conj.values().index(word[-2:])] 
+				print "The verb is %s. It is a %s, 4th conj" % (word, ending)
+				return None
+			elif word[-3:] in des_ind_pres_4_conj.values():
+				ending = des_ind_pres_4th_conj.keys()[des_ind_pres_4_conj.values().index(word[-3:])] 
 				print "The verb is %s. It is a %s." % (word, ending)
 				return None
-			elif word[-3:] in des_pres_ind_pres_2_conj.values():
-				ending = des_pres_ind_pres_2_conj.keys()[des_pres_ind_pres_2_conj.values().index(word[-3:])] 
+			elif word[-4:] in des_ind_pres_4_conj.values():
+				ending = des_ind_pres_4th_conj.keys()[des_ind_pres_4_conj.values().index(word[-4:])] 
 				print "The verb is %s. It is a %s." % (word, ending)
 				return None
-			elif word[-4:] in des_pres_ind_pres_2_conj.values():
-				ending = des_pres_ind_pres_2_conj.keys()[des_pres_ind_pres_2_conj.values().index(word[-4:])] 
-				print "The verb is %s. It is a %s." % (word, ending)
-				return None
-			
+		
+
 def find_subject():
 	global des_subst_first_decl
 	global des_subst_second_decl
@@ -99,8 +122,8 @@ def analyze_sentence():
 	find_subject()
 	find_object()
 
-# def exit_analyzer():
-# 	print "Oh. Sorry you're leaving. Thank you for working with me. Come and visit again soon :-)"	
+def exit_analyzer():
+	print "Oh. Sorry you're leaving. Thank you for working with me. Come and visit again soon :-)"	
 
 def menu():
 	print "1 - Find verb \n 2 - Find Subject \n 3 - Find Object \n 4 - Analyze word \n 5 - Analyze sentence \n 6 - Exit" 
